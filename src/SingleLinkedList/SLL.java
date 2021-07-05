96d59728c9d4d8cd7f2c4906c72377d999ccd827
Anmol17Agarwal <anmol.agarwal_iot18@gla.ac.in>
2021-07-05 23:53:38 +0530

    Initial commit

---
+++ src/SingleLinkedList/SLL.java
@@ -0,0 +1,38 @@
+package SingleLinkedList;
+
+public class SLL
+{
+    private ListNode head;
+    private static class ListNode {
+
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+        public void display()
+        {
+            ListNode current = head;
+            while(current!=null)
+            {
+                System.out.print(current.data+" --> ");
+                current=current.next;
+            }
+            System.out.print("null");
+        }
+
+        public static void main(String[] args)
+        {
+            SLL sll = new SLL();
+            sll.head = new ListNode(10);
+            ListNode second = new ListNode(20);
+            ListNode third = new ListNode(30);
+            ListNode fourth = new ListNode(40);
+    }
+    }
+
+
