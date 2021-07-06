e92799fa19f1c04109b1a956bb312316014c5642
Anmol17Agarwal <anmol.agarwal_iot18@gla.ac.in>
2021-07-06 21:57:30 +0530

    created length method to count length of linkedlist.

--- src/SingleLinkedList/SLL.java
+++ src/SingleLinkedList/SLL.java
@@ -7,43 +7,64 @@ package SingleLinkedList;
 public class SLL
 {
     private ListNode head; //initializing head as first element of LinkedList
-    private static class ListNode { //Define ListNode
+    private static class ListNode //Define ListNode
+    {
 
-        private int data;//initialise data as int type
-        private ListNode next;// initialise next to linked with next node
+        private int data; //initialise data as int type
+        private ListNode next; // initialise next to linked with next node
 
-        public ListNode(int data) {//constructor of listNode taking integer value as data
-            this.data = data;// updating the value of data everytime
+        public ListNode(int data) //constructor of listNode taking integer value as data
+        {
+            this.data = data; // updating the value of data everytime
             this.next = null;
         }
     }
 
-        public void display()// create display method to rpint linkedlist
+        public void display() // create display method to rpint linkedlist
         {
-            ListNode current = head;// initialise current and store head as first value of SLL
-            while(current!=null)//loop till current is not equal to null
+            ListNode current = head; // initialise current and store head as first value of SLL
+            while(current!=null) //loop till current is not equal to null
             {
-                System.out.print(current.data+" --> ");//print statement
-                current=current.next;// updating current node using next
+                System.out.print(current.data+" --> "); //print statement
+                current=current.next; // updating current node using next
             }
             System.out.print("null");
         }
 
+
+        public int length()// create length method
+        {
+            int count=0; // initizalise count
+            ListNode current = head;
+            if(current == null) // check if head is null return length as zero
+            {
+                 return 0;
+            }
+            while(current!=null)
+            {
+                    count++;
+                    current = current.next;
+            }
+            return count;
+        }
+
         public static void main(String[] args)
         {
-            SLL sll = new SLL();// create object
-            ListNode first = new ListNode(10);// default value of first element
-            ListNode second = new ListNode(20);// default value of second element
-            ListNode third = new ListNode(30);// default value of third element
-            ListNode fourth = new ListNode(40);// default value of fourth element
+            SLL sll = new SLL(); // create object
+            ListNode first = new ListNode(10); // default value of first element
+            ListNode second = new ListNode(20); // default value of second element
+            ListNode third = new ListNode(30); // default value of third element
+            ListNode fourth = new ListNode(40); // default value of fourth element
 
-            sll.head = first;//storing first value as head
-            first.next = second;//linking node using next
-            second.next = third;//linking node using next
-            third.next = fourth;//linking node using next
+            sll.head = first; //storing first value as head
+            first.next = second; //linking node using next
+            second.next = third; //linking node using next
+            third.next = fourth; //linking node using next
 
             sll.display();
-    }
+            System.out.println();
+            System.out.println("Length of LinkedList = "+sll.length());
+        }
     }
 
 
